77b84320795514651c3b855bb2d4381be08c23b6
Pitrified <nobilipietro@gmail.com>
2017-12-19 19:48:30 +0100

    CollassaNodi aggiornato

--- CollassaNodi.py
+++ CollassaNodi.py
@@ -9,13 +9,17 @@ def printType(obj):
   print obj, " - ", type(obj)
   
 #cerca l'ID nei set del dizionario e restituisce il primo ID del set giusto
+#deve sempre trovare l'ID nel dizionario
 def trovaSostituto(id):
   for entry in dautori:
     #printType(dautori[entry])
     if id in dautori[entry]:
-      print dautori[entry]
+      #print dautori[entry]
+      return dautori[entry][0]
+  return "NOTFOUND!"
 
-fautori = open("AutoriCollabOrdinatiNOMEpoiIDridotto.txt", "r")
+#fautori = open("AutoriCollabOrdinatiNOMEpoiIDridotto.txt", "r")
+fautori = open("AutoriPadovaniOrdinatiNOMEpoiID.txt", "r")
 #with open('AutoriCollabOrdinatiNOMEpoiID.txt', 'r') as fautori
 
 dautori = {}
@@ -24,18 +28,40 @@ for line in fautori:
   pezzi = line.split("\t", 1)
   pezzi[1] = pezzi[1].rstrip()          #tolgo il \n
   if not dautori.has_key(pezzi[1]):
-    dautori.update({pezzi[1]:set()})    #creo un set vuoto per la chiave pezzi[1]
+    dautori.update({pezzi[1]:[]})       #creo una lista vuota per la chiave pezzi[1]
   
-  dautori[pezzi[1]].add(pezzi[0])       #aggiungo l'ID al set  
+  dautori[pezzi[1]].append(pezzi[0])    #aggiungo l'ID alla lista  
 fautori.close()
 
 print dautori
 
-fedge = open("EdgeDEIPesatiRidotto.txt", "r")
-fedgenuovi = open("EdgeDEIPesatiUnificati.txt", "w")
+#fedge = open("EdgeDEIPesatiRidotto.txt", "r")
+fedge = open("EdgePadovaniCompletiPesati.txt", "r")
+fedgenuovi = open("EdgePadovaniCompletiPesatiUnificati.txt", "w")
+dpesato = {}
 
 for line in fedge:
-  pezzi = line.split("\t", 1)
-  trovaSostituto(pezzi[0])
+  pezzi = line.split("\t")
+  #riga = trovaSostituto(pezzi[0])+"\t"+trovaSostituto(pezzi[1])+"\t"+pezzi[2]
+  #fedgenuovi.write(riga)
   
+  id0 = trovaSostituto(pezzi[0])
+  id1 = trovaSostituto(pezzi[1])
   
+  if id0<id1:
+    coppia = id0+"\t"+id1
+  else:
+    coppia = id1+"\t"+id0
+  
+  if not dpesato.has_key(coppia):
+    dpesato.update({coppia:int(pezzi[2].rstrip())})
+  else:
+    dpesato[coppia]+=int(pezzi[2].rstrip())
+    
+print dpesato
+
+for entry in dpesato:
+  fedgenuovi.write(entry+"\t"+str(dpesato[entry])+"\n")
+  
+fedge.close()
+fedgenuovi.close()
