f2f20950de21abca5e943f8e6d7375c7befa1d80
unknown <nobilipietro@gmail.com>
2017-12-30 13:04:54 +0100

    PreparaPerGephi - cambio nome cartella

---
+++ Verifiche_Test/PreparaPerGephi.py
@@ -0,0 +1,45 @@
+#! python2
+
+import re
+
+def ptype(obj):
+  print obj, " - ", type(obj)
+
+def processaNodi(nome, est=".txt"):
+  fullname = dir+"\\"+nome+est
+  finput = open(fullname, "r")
+  strInput = finput.read()
+  finput.close()
+
+  fullnameout = dir+"\\"+nome+".tsv"
+  foutput = open(fullnameout, "w")
+  foutput.write("id\tLabel\n")          #aggiunge la riga iniziale
+  foutput.write(strInput)
+  foutput.close()
+  
+def processaEdge(nome, est=".txt"):
+  fullname = dir+"\\"+nome+est
+  finput = open(fullname, "r")
+  strInput = finput.read()
+  finput.close()
+
+  strOutput = strInput.replace("\n","\tUndirected\n")     #gli edge sono Undirected
+  #strOutput = re.sub("\n","\tUndirected\n", strInput)
+
+  fullnameout = dir+"\\"+nome+".tsv"
+  foutput = open(fullnameout, "w")
+  foutput.write("Source\tTarget\tWeight\tType\n")         #aggiunge la riga iniziale
+  foutput.write(strOutput)
+  foutput.close()
+
+
+dir = r'C:\Users\Test\Documents\Tesi\authorship-network\Versione2'    #cartella in cui lavora
+
+processaNodi("AutoriPadovaniAmpiUnificati")
+processaNodi("AutoriCollabAmpiUnificati")
+processaNodi("AutoriPadovaniAmpi")
+processaNodi("AutoriCollabAmpi")
+
+processaEdge("EdgePadovaniCompletiPesatiAmpiUnificati")
+processaEdge("EdgeCollabPesatiAmpiUnificati")
+
