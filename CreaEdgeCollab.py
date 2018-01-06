583ed62c5754cdbf46ba8a1304349ceae8cac77e
unknown <nobilipietro@gmail.com>
2018-01-06 14:48:07 +0100

    Controllo duplicati negli IDaut dei paper
    Identifica i paper che genereranno selfloop a causa di un singolo IDaut
    associato allo stesso paper
    Non controlla in alcun modo i nomi degli autori

--- CreaEdgeCollab.py
+++ CreaEdgeCollab.py
@@ -9,10 +9,10 @@ CARTELLA = "Versione2\\"
 def ptype(obj):
   print obj, " - ", type(obj)
 
-#carico il dizionario {paper:[a1,a2,a4,a3]}
-dpaa = {}
-#fpaa = open(CARTELLA+"PapAutAffDEIampi.txt")
-fpaa = open(CARTELLA+"PaperPadovaniCompletiAmpi.txt")
+
+dpaa = {} #carico il dizionario {paper:[a1,a2,a4,a3]}
+fpaa = open(CARTELLA+"PapAutAffDEIampi.txt")
+#fpaa = open(CARTELLA+"PaperPadovaniCompletiAmpi.txt")
 for line in fpaa:
   pezzi = line.split("\t")
   if not dpaa.has_key(pezzi[0]): dpaa.update({pezzi[0]:[]})     #se non c'e' la chiave creo lista vuota
@@ -20,6 +20,17 @@ for line in fpaa:
   #ptype(dpaa[pezzi[0]])
 fpaa.close()
 #print dpaa
+
+#controllo duplicati nei IDaut dei paper
+#check solo sugli ID, non considera i nomi
+fpaperloop = open(CARTELLA+"PaperAADALoopBis.txt", "w")
+for entry in dpaa:
+  if len(dpaa[entry]) <> len(set(dpaa[entry])):
+    print "paper con ID duplicati nella lista:", entry, "lista", dpaa[entry]
+    fpaperloop.write("paper con ID duplicati nella lista: "+ entry + " lista: " + str(dpaa[entry]) + "\n")
+    
+fpaperloop.close()
+  
 
 #creo edge a1a2,a1a4,a1a3,a2a4,a2a3,a3a4 pesati
 dedgepesati = {}
@@ -35,12 +46,13 @@ for entry in dpaa:
       else: coppia = ledge[j]+"\t"+ledge[i]
       #print coppia
       if not dedgepesati.has_key(coppia): dedgepesati.update({coppia:1})
-      else: dedgepesati[coppia]+=1
+      else: dedgepesati[coppia]+=1      
       j+=1
     i+=1
 #print dedgepesati
 
-#fedge = open(CARTELLA+"EdgeCollabPesatiAmpi.txt", "w")
-fedge = open(CARTELLA+"EdgePadovaniCompletiPesatiAmpi.txt", "w")
+fedge = open(CARTELLA+"EdgeCollabPesatiAmpiBis.txt", "w")
+#fedge = open(CARTELLA+"EdgePadovaniCompletiPesatiAmpiBis.txt", "w")
 for entry in dedgepesati:
   fedge.write(entry+"\t"+str(dedgepesati[entry])+"\n")
+fedge.close()
