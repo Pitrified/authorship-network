38784b297e960d1797aee66c4fea9fb81da78a43
unknown <nobilipietro@gmail.com>
2018-01-24 14:51:06 +0100

    Estrae nomi di afferenti un utf-8

--- EstraiPersoneDaUnipd.py
+++ EstraiPersoneDaUnipd.py
@@ -10,7 +10,9 @@ import os
 
 
 def estraiPersoneDaUnipd(pfPersone, url):
-  htmldoc = urlopen(url).read()
+  # htmldoc = unicode(urlopen(url).read(), 'utf-8')
+  with open('PersoneSPGIhtml.html', 'rb') as f:
+    htmldoc = f.read()
   soup = BeautifulSoup(htmldoc, 'html.parser')
 
   with open(pfPersone, 'wb' ) as fPersone:
@@ -20,20 +22,22 @@ def estraiPersoneDaUnipd(pfPersone, url):
         nome = spans[0].string.lower()
         cognome = spans[1].string.lower()
         print 'nome: ', nome, '\tcognome: ', cognome
-        fPersone.write('{} {}\r\n'.format(nome, cognome))
+        fPersone.write('{} {}\r\n'.format(nome, cognome).decode('utf-8'))
     
 
 if __name__ == '__main__':
   print 'This program is EstraiPersoneDaUnipd, being run by itself' 
   
   url = 'http://www.dsfarm.unipd.it/category/ruoli/personale-docente'
+  url = 'https://www.biologia.unipd.it/dipartimento/persone/docenti/'
+  url = 'http://www.spgi.unipd.it/category/ruoli/personale-docente'
   # url = 'https://www.dei.unipd.it/lista-docenti'
   
   celaborati = 'Versione3_Multi\\'
   sub = 'Amplia\\'
   sub = ''
   if not os.path.exists(celaborati + sub): os.makedirs(celaborati + sub)
-  tag = 'CTF'
+  tag = 'SPGI'
   pfPersone = celaborati + sub + 'PersoneNomi' + tag + '.txt'
   
   estraiPersoneDaUnipd(pfPersone, url)
